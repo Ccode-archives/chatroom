52e563c47d98c3b8da06a70d825297d4c6614706
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-08-22 13:37:25 -0900

    Create client.py

---
+++ client/client.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# Python program to implement client side of chat room. 
+import socket 
+import select 
+import sys 
+def delete_last_line():
+    #cursor up one line
+    sys.stdout.write('\x1b[1A')
+    #delete last line
+    sys.stdout.write('\x1b[2K')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+if len(sys.argv) != 3: 
+    print ("Correct usage: script, IP address, port number")
+    exit() 
+IP_address = str(sys.argv[1]) 
+Port = int(sys.argv[2]) 
+server.connect((IP_address, Port)) 
+running = True
+while running: 
+  
+    # maintains a list of possible input streams 
+    sockets_list = [sys.stdin, server] 
+  
+    """ There are two possible input situations. Either the 
+    user wants to give manual input to send to other people, 
+    or the server is sending a message to be printed on the 
+    screen. Select returns from sockets_list, the stream that 
+    is reader for input. So for example, if the server wants 
+    to send a message, then the if condition will hold true 
+    below.If the user wants to send a message, the else 
+    condition will evaluate as true"""
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+  
+    for socks in read_sockets: 
+        if socks == server: 
+            message = socks.recv(2048) 
+            print (message) 
+        else: 
+            message = sys.stdin.readline()
+            delete_last_line()
+            if not str(message) == "exit\n":
+                server.send(message)
+            else:
+                hostname = socket.gethostname()
+                local_ip = socket.gethostbyname(hostname)
+                server.send(local_ip + " Has left the server")
+                running = False
+                break
+            sys.stdout.write("<You>") 
+            sys.stdout.write(message) 
+            sys.stdout.flush() 
+server.close()
